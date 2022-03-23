c6d1dc86af301fdbb53e8b72fbc46e3d6c2010a8
Thiago López <thiagolopezalderete@hotmail.com>
2022-03-23 12:08:15 -0300

    Añadido el path.resolve

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const publicPath = path.resolve(__dirname, "./public");
 
 // Room 4
 
 app.listen(process.env.PORT || 3030, () =>
   console.log("Web levantada con exito en el puerto 3030")
 );
-app.use(express.static("public"));
+app.use(express.static(publicPath));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"));
